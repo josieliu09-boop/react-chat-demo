3254304cae30582da7dff5bcf32283883fa867e9
你说你是小狗 <869744575@qq.com>
2026-04-26 21:40:27 +0800

    update README

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,8 @@ import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <h1>刘倩做的AI Chat Demo</h1>
-            <p>一个简单的 React+TypeScript 聊天框练习</p>
+            <h1>AI Chat Demo</h1>
+            <p>React+TypeScript</p>
         </div>
     )
 }
